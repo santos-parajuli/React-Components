bf2b0eb65edbdd495bdd3b0cf9722ed1aaf77933
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2021-06-14 14:15:30 +0545

    Update NavBar.js

--- navBar/NavBar.js
+++ navBar/NavBar.js
@@ -15,23 +15,19 @@ export default class navBar extends Component {
         //       }},
         //     }
         // }}
-        this.values = {
-          brandName:" Santosh Sakar Pustak Pasal",
-          navItems :{
-            nav1:{name:"Home",goto:"/"},
-            // nav2:{name:"Buy",links:{
-            //   subLink1:{name:"Books",goto:"/books"},
-            //   subLink2:{name:"Other Items",goto:"/stationeryItems"},
-            // }},
-            nav2:{name:"Buy Books",goto:"/books"},
+                this.values = {
+          brandName:" Project",
+          navItems :[
+            {name:"Home",goto:"/",type:"link"},
+            {name:"Buy",type:"dropDown",links:[
+              {name:"Books",goto:"/books"},
+              {name:"Other Items",goto:"/stationeryItems"},
+            ]},
+            {name:"Buy Books",goto:"/books",type:"button"},
 
-            // nav3:{name:"Orders",goto:"/order"},
-
-          }
+          ]
       }
     }
- 
-
     render() {
         return (
             <div className="navigation">
@@ -43,20 +39,21 @@ export default class navBar extends Component {
                   <div className="nav-mobile"><a id="nav-toggle" className={`${this.state.toggle?"active":""}`} onClick={()=>{this.setState({toggle:!this.state.toggle})} } href="#!"><span></span></a></div>
                   <ul className={`nav-list `} style={ this.state.toggle ? { display:'block'} : {display:'none'}}  >
                     {
-                      Object.keys(this.values.navItems).map((nav)=>{
-                        if(this.values.navItems[nav].links){
+                      this.values.navItems.map((nav)=>{
+                        console.log(nav.links)
+                        if(nav.links){
                           return <li key={nav}>
                                     <a href="#1" onClick={() => { 
-                                      this.setState({open:{[nav]:(this.state.open[nav])?!this.state.open[nav]:true}})}}>{this.values.navItems[nav].name} 
+                                      this.setState({open:{[nav]:(this.state.open[nav])?!this.state.open[nav]:true}})}}>{nav.name} 
                                     </a>
                                     <ul className="nav-dropdown" style={ this.state.open[nav] ? { display:'block'} : {display:'none'}}>
-                                    {Object.keys(this.values.navItems[nav].links).map((subNav)=>{
-                                      return<li key={subNav}><a href={this.values.navItems[nav].links[subNav].goto}>{this.values.navItems[nav].links[subNav].name}</a></li>
+                                    {nav.links.map((subNav)=>{
+                                      return<li key={subNav}><a href={subNav.goto}>{subNav.name}</a></li>
                                     })}  
                                     </ul>
                                   </li>
                         }else{
-                          return <li key={nav}> <a href={this.values.navItems[nav].goto}>{this.values.navItems[nav].name}</a> </li>
+                          return <li key={nav}> <a href={nav.goto}>{nav.name}</a> </li>
                         }
                       })
                     } 
